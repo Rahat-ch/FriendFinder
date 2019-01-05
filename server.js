2812691a4bbbfe2dfa4dea7a895f27b6306afc97
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-01-05 13:38:45 -0500

    home and survey routes working

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require("express");
-var app = express();
-var bodyParser = require('body-parser')
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser')
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 // create application/json parser
-var jsonParser = bodyParser.json()
+const jsonParser = bodyParser.json()
 
 // create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // parse various different custom JSON types as JSON
 app.use(bodyParser.json({ type: 'application/*+json' }))
@@ -18,6 +18,8 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 
 // parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
+
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
